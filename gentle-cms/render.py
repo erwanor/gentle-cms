7b9c5e8d9cd9ab55c8f2b546684409d1f919dbcb
aaronwinter <eatounno@uwaterloo.ca>
2015-01-10 18:09:17 +0000

    load configuration from yaml to hashtable

--- gentle-cms/render.py
+++ gentle-cms/render.py
@@ -8,21 +8,22 @@ import yaml
 ##################
 
 def input_checking(inputArgs):
-    if verify_length(inputArgs, 2) is False or \
-    verify_extension(inputArgs, ".md") is False:
+    if check_length(inputArgs, 2) is False or \
+       check_extension(inputArgs, ".md") is False:
         print "usage: render.py index.md"
         sys.exit()
     else:
             return inputArgs[1]
 
 def check_length(inputArgs, length):
-    if len(inputArgs) != length:
-        return False
-    else:
+    if len(inputArgs) == length:
         return True
+    else:
+        return False
 
-def check_extension(fileToCheck, extension):
+def check_extension(inputArgs, extension):
     extensionLength = len(extension)
+    fileToCheck = inputArgs[1]
     if fileToCheck[-extensionLength:] == extension:
         return True
     else:
@@ -35,16 +36,18 @@ def enum(*sequential, **named):
     return type('Enum', (), enums)
 
 def load_configuration():
-    CONFIG_FILE = "config.yaml"
-    stream      = open(CONFIG_FILE, 'r')
-    config_link = yaml.load(stream)
-    for key, value in doc.items():
-        print key, '->', value
-        print '\n'
-
+    CONFIG_FILE  = "config.yaml"
+    config_data  = {}
+    stream       = open(CONFIG_FILE, 'r')
+    config_steam = yaml.load(stream)
+    for key, value in config_steam.items():
+        config_data.update({key : value})
+    return config_data
 #################
 
 FORMAT_TYPE = enum(HEADER = 0, SUB_HEADER = 1, PARAGRAPH = 2, IMAGE = 3,
                    YOUTUBE_VIDEO = 4, LINK = 5)
 
 MARKDOWN_SOURCE = input_checking(sys.argv)
+
+configuration = load_configuration()
