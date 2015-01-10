8fcb72e690971170271c0e62555744d65374b9c5
aaronwinter <eatounno@uwaterloo.ca>
2015-01-10 17:32:46 +0000

    add yaml support + re-organize input checking

--- gentle-cms/render.py
+++ gentle-cms/render.py
@@ -1,10 +1,25 @@
 #!/usr/bin/env python
 
 import sys
+import yaml
 
 ##########
 #  HELPERS
 ##################
+
+def input_checking(inputArgs):
+    if verify_length(inputArgs, 2) is False or \
+    verify_extension(inputArgs, ".md") is False:
+        print "usage: render.py index.md"
+        sys.exit()
+    else:
+            return inputArgs[1]
+
+def check_length(inputArgs, length):
+    if len(inputArgs) != length:
+        return False
+    else:
+        return True
 
 def check_extension(fileToCheck, extension):
     extensionLength = len(extension)
@@ -13,28 +28,23 @@ def check_extension(fileToCheck, extension):
     else:
         return False
 
-def verify_input(inputArgs):
-    if len(inputArgs) != 2:
-        print "usage: render.py index.md"
-        sys.exit()
-    else:
-        MARKDOWN_FILE = inputArgs[1]
-        check = check_extension(MARKDOWN_FILE, ".md")
-        if check is False:
-            print "The input extension is not .md"
-            sys.exit()
-        else:
-            return MARKDOWN_FILE
-
 def enum(*sequential, **named):
     enums = dict(zip(sequential, range(len(sequential))), **named)
     reverse = dict((value, key) for key, value in enums.iteritems())
     enums['reverse_mapping'] = reverse
     return type('Enum', (), enums)
 
+def load_configuration():
+    CONFIG_FILE = "config.yaml"
+    stream      = open(CONFIG_FILE, 'r')
+    config_link = yaml.load(stream)
+    for key, value in doc.items():
+        print key, '->', value
+        print '\n'
+
 #################
 
 FORMAT_TYPE = enum(HEADER = 0, SUB_HEADER = 1, PARAGRAPH = 2, IMAGE = 3,
                    YOUTUBE_VIDEO = 4, LINK = 5)
 
-SOURCE = verify_input(sys.argv)
+MARKDOWN_SOURCE = input_checking(sys.argv)
