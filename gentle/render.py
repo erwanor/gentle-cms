ce61565408abaf875b5d8d50ccca44fca1d5db72
aaronwinter <eatounno@uwaterloo.ca>
2015-01-24 20:41:05 -1000

    Modify class structure and split methods into three new classes

--- gentle/render.py
+++ gentle/render.py
@@ -4,9 +4,8 @@ from sys import argv
 from yaml import load
 from re import match
 
-##########
-#  HELPERS
-##################
+FORMAT_TYPE = Utils.enum(HEADER = 0, SUB_HEADER = 1, PARAGRAPH = 2, UNKNOWN = 3)
+LINK_TYPE   = Utils.enum(IMAGE  = 0, YOUTUBE    = 1, LINK = 2, UNKNOWN = 3)
 
 class Utils:
     def __init__(self):
@@ -44,6 +43,49 @@ class Utils:
         enums['reverse_mapping'] = reverse
         return type('Enum', (), enums)
 
+    class Identify:
+        def __init__(self):
+            pass
+
+        @staticmethod
+        def is_header(entry):
+            matched = match('^[\#]{1}([a-z-A-Z-0-9- ._:)($\[\]!~@,-]{1,})$', entry)
+            return matched is not None
+
+        @staticmethod
+        def is_sub_header(entry):
+            matched = match('^[\#]{2}([a-z-A-Z-0-9- ._:)($\[\]!~@,-]{1,})$', entry)
+            return matched is not None
+
+        @staticmethod
+        def is_paragraph(entry):
+            matched = match('^[\-]{3}([a-z-A-Z-0-9- ._:)($\[\]!~@,-]{1,})[\-]{3}$', entry)
+            return matched is not None
+
+        @staticmethod
+        def check_entry_type(entry):
+            if self.is_header(entry) is True:
+                return FORMAT_TYPE.HEADER
+            elif self.is_sub_header(entry) is True:
+                return FORMAT_TYPE.SUB_HEADER
+            elif self.is_paragraph(entry) is True:
+                return FORMAT_TYPE.PARAGRAPH
+            else:
+                return FORMAT_TYPE.UNKNOWN
+
+        @staticmethod
+        def check_match_type(match):
+            if self.is_image(match.group(2)) is True:
+                return LINK_TYPE.IMAGE
+            elif self.is_youtube(match.group(2)) is True:
+                return LINK_TYPE.YOUTUBE
+            else:
+                return LINK_TYPE.LINK
+
+class Configuration:
+    def __init__(self):
+        pass
+
     @staticmethod
     def load_configuration():
         CONFIG_FILE  = "config.yaml"
@@ -54,72 +96,28 @@ class Utils:
             config_data.update({key : value})
         return config_data
 
-    @staticmethod
-    def is_header(entry):
-        matched = match('^[\#]{1}([a-z-A-Z-0-9- ._:)($\[\]!~@,-]{1,})$', entry)
-        return matched is not None
 
-    @staticmethod
-    def is_sub_header(entry):
-        matched = match('^[\#]{2}([a-z-A-Z-0-9- ._:)($\[\]!~@,-]{1,})$', entry)
-        return matched is not None
-
-    @staticmethod
-    def is_paragraph(entry):
-        matched = match('^[\-]{3}([a-z-A-Z-0-9- ._:)($\[\]!~@,-]{1,})[\-]{3}$', entry)
-        return matched is not None
-
-#################
-
-FORMAT_TYPE = Utils.enum(HEADER = 0, SUB_HEADER = 1, PARAGRAPH = 2, UNKNOWN = 3)
-LINK_TYPE   = Utils.enum(IMAGE  = 0, YOUTUBE    = 1, LINK = 2, UNKNOWN = 3)
-
-class Render:
+class PreProcessing:
     def __init__(self, markdown_source):
         self.markdown_source = markdown_source
-        self.configuration = Utils.load_configuration()
+        self.configuration = Configuration.load_configuration()
 
     def scan_source(self, source):
         markdown_data = []
         with open(source) as stream:
             for line in stream:
-                markdown_data.append((Render.check_entry_type(line), line))
+                markdown_data.append((Utils.Identify.check_entry_type(line), line))
                 print line
         print markdown_data
 
     def scan_entry(self, entry):
-        match = Render.match_links(entry)
+        match = Utils.Identify.match_links(entry)
         if match is not None:
-            Render.check_match_type(match)
+            Utils.Identify.check_match_type(match)
         return None
 
     @staticmethod
     def match_links(line):
         matched = match('\[([a-zA-Z]*)\]\((https?:\/\/[a-zA-Z-0-9-.-\/$?&=%$]*)\)', line)
         return matched
-
-    @staticmethod
-    def check_entry_type(entry):
-        if Utils.is_header(entry) is True:
-            return FORMAT_TYPE.HEADER
-        elif Utils.is_sub_header(entry) is True:
-            return FORMAT_TYPE.SUB_HEADER
-        elif Utils.is_paragraph(entry) is True:
-            return FORMAT_TYPE.PARAGRAPH
-        else:
-            return FORMAT_TYPE.UNKNOWN
-    
-    @staticmethod
-    def check_match_type(match):
-        if Utils.is_image(match.group(2)) is True:
-            return LINK_TYPE.IMAGE
-        elif Utils.is_youtube(match.group(2)) is True:
-            return LINK_TYPE.YOUTUBE
-        else:
-            return LINK_TYPE.LINK
-
-
-    @staticmethod
-    def generate_html(entry_type):
-        pass
 
