a12ec68ce4f908e7a5a78f0cc6982e2c2d6d84d6
aaronwinter <eatounno@uwaterloo.ca>
2015-01-26 10:16:54 -1000

    Fix and modify multiple method calls and class signature

--- gentle/engine.py
+++ gentle/engine.py
@@ -15,7 +15,7 @@ ENTRY_TYPE = enum(HEADER = 0, SUB_HEADER = 1, PARAGRAPH = 2, UNKNOWN = 3)
 class Utils:
     def __init__(self):
         pass
-    
+
     @staticmethod
     def input_checking(inputArgs):
         if check_length(inputArgs, 2) is False or \
@@ -62,11 +62,11 @@ class Utils:
 
         @staticmethod
         def check_entry_type(entry):
-            if self.is_header(entry) is True:
+            if Utils.Identify.is_header(entry) is True:
                 return ENTRY_TYPE.HEADER
-            elif self.is_sub_header(entry) is True:
+            elif Utils.Identify.is_sub_header(entry) is True:
                 return ENTRY_TYPE.SUB_HEADER
-            elif self.is_paragraph(entry) is True:
+            elif Utils.Identify.is_paragraph(entry) is True:
                 return ENTRY_TYPE.PARAGRAPH
             else:
                 return ENTRY_TYPE.UNKNOWN
@@ -75,8 +75,7 @@ class Configuration(object):
     def __init__(self, config="config.yaml"):
         self.CONFIG_FILE = config
 
-    @staticmethod
-    def load_configuration():
+    def load_configuration(self):
         config_data  = {}
         stream       = open(self.CONFIG_FILE, 'r')
         config_steam = load(stream)
@@ -89,13 +88,14 @@ class PreProcessing:
     def __init__(self, markdown_source):
         self.markdown_source = markdown_source
 
-    def scan_source(self, source):
+    def scan_source(self):
         processed_data = []
-        with open(source) as stream:
+        with open(self.markdown_source) as stream:
             for block in stream:
-                entry_type = Utils.Identify.check_match_type(block)
+                entry_type = Utils.Identify.check_entry_type(block)
                 proc_entry = Processing.superstructure((entry_type, block))
                 processed_data.append((proc_entry, entry_type))
+                print processed_data
         return processed_data
 
     def scan_entry(self, preprocessed_data):
@@ -105,15 +105,17 @@ class PreProcessing:
             style_processed_entry = Processing.style(raw_entry)
             processed_entry = Processing.links(style_processed_entry)
             processed_data.append((entry_type, processed_entry))
+            print processed_data
         return processed_data
 
 class Processing:
     def __init__(self):
-        self.configuration = Configuration.load_configuration()
-        self.header = '<html><head><title></title>\
-        <link type="text/css" rel="stylesheet" href="style/.css" />\
+        self.configuration = Configuration().load_configuration()
+        self.html_header = '<html><head><title>' + self.configuration["blog_name"]  + ' - ' \
+        + self.configuration["blog_description"]  + '</title>\
+        <link type="text/css" rel="stylesheet" href="' + self.configuration["theme"]  + '" />\
         </head><body>'
-        self.footer = '</body></html>'
+        self.html_footer = '</body></html>'
 
     @staticmethod
     def compile(fully_processed_data):
@@ -121,7 +123,7 @@ class Processing:
         body = ""
         for structure in fully_processed_data:
             body += ' ' + structure
-            return Process.header + body + Process.footer
+            return Process.header_html + body + Process.footer_html
     @staticmethod
     def superstructure(data):
         structure_type, structure = data
@@ -183,6 +185,7 @@ class Processing:
     def bold(self, data):
         pattern = "\*[a-z-A-Z-0-9\?\!\.\*\[\]\(\)\:\/\?\&\=\!\;\ ]*\*"
         repl    = "<b>\\1</b>"
+        print type(pattern), type(repl), type(data)
         processed = sub(pattern, repl, data)
         return processed
 
@@ -198,14 +201,15 @@ class Processing:
         processed = sub(pattern, repl, data)
         return processed
 
-    @staticmethod
-    def compile(processed_data):
-        return processed_data
 
-class Main:
-    def __init__(self, configuration_file = "config.yaml"):
-        self.configuration_file = configuration_file
-        pass
+class Main(object):
+    def __init__(self, path_to_markdown=None):
+        self.path_to_markdown = path_to_markdown
 
-    def create_article(self, path_to_markdown_article):
-        Configuration.load_configuration(self.configuration_file)
+    def create_article(self):
+        PreProcess = PreProcessing(self.path_to_markdown)
+        unprocessed_data = PreProcess.scan_source()
+        data = []
+        for structure in unprocessed_data:
+            data.append(PreProcess.scan_entry(unprocessed_data))
+        print data
